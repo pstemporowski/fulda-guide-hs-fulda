c153a0125f8b7d0272fc756902144cb937006f73
Patryk <stemper0123@gmail.com>
2023-01-06 14:47:54 +0100

    AR

--- lib/sandbox_screens/3dViewer_screen.dart
+++ lib/sandbox_screens/3dViewer_screen.dart
@@ -11,13 +11,22 @@ class ModelView extends StatelessWidget {
     //src: "lib/assets/Mensa.glb"
     return Scaffold(
       appBar: AppBar(title: Text("Model Viewer")),
-      body: ModelViewer(src: 'lib/assets/Mensa.glb',
+      body: ModelViewer(src: 'lib/assets/G46.glb',
         alt: "A 3D model of an astronaut",
-        autoRotate: true,
-        disableZoom: true,
-        cameraControls: true,
+        autoRotate: false,
+        cameraControls: false,
+        ar: true,
+        arScale: ArScale.auto,
+        arPlacement: ArPlacement.floor,
+        arModes: ['scene-viewer', 'webxr', 'quick-look'],
+        backgroundColor: Colors.transparent,
+        cameraOrbit: "90deg 90deg 100",
+        maxCameraOrbit: "115deg 90deg 100",
+        minCameraOrbit: "65deg 90deg 90deg",
         autoPlay: true,
+
       ),
     );
   }
+
 }
